ec25d3e4bd1d9b570af890c19767e177cc63c06a
Tadeu <tbernacchi@gmail.com>
2018-05-14 01:02:36 -0300

    Added 3 instances

--- aws/instances.tf
+++ aws/instances.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "teste" { 
+resource "aws_instance" "tadeu-teste" {
+  count                       = 3
   ami                         = "ami-b0b7e3dc"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   key_name                    = "tadeu-new-18"
-  vpc_security_group_ids  = ["${aws_security_group.my_ssh.id}"]
-
+  vpc_security_group_ids      = ["${aws_security_group.my_ssh.id}"]
 
   tags {
-    name = "tadeu-disco"
+    Name = "tadeu-teste-${count.index}"
   }
 }
